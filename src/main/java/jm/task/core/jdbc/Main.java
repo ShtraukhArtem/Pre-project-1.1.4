39bea3bbc47816b1b61a27584c1d4df4545cd2b5
ShtraukhArtem <arshtraukh@yandex.ru>
2024-12-20 19:32:32 +0500

    Commit 1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,19 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserServiceImpl userServiceImpl = new UserServiceImpl();
+
+        userServiceImpl.createUsersTable();
+        userServiceImpl.saveUser("Mike", "Ivanov", (byte) 32);
+        userServiceImpl.saveUser("Sveta", "Petrova", (byte) 24);
+        userServiceImpl.saveUser("Denis", "Orlov", (byte) 29);
+        userServiceImpl.saveUser("Alex", "Lebedev", (byte) 21);
+        userServiceImpl.getAllUsers();
+        userServiceImpl.cleanUsersTable();
+        userServiceImpl.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,26 @@
 package jm.task.core.jdbc.util;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/katadbtest";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "pass";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            System.out.println("Connected to the database successfully");
+        } catch (SQLException e) {
+            System.out.println("Failed to load JDBC driver");
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+
 }
